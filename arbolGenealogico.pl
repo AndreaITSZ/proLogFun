9b02727b59e261f8e5b96f3bb6747b7b9e519387
AndreaITSZ <lunaandrea301@gmail.com>
2016-09-14 22:11:19 -0500

    Arbol Genealogico

---
+++ arbolGenealogico.pl
@@ -0,0 +1,24 @@
+padre(cristobal,guadalupe).
+padre(cristobal,lucio).
+padre(cristobal,carmen).
+padre(cristobal,guero).
+padre(cristobal,lety).
+padre(cristobal,alfredo).
+padre(antonio, david).
+padre(antonio, guero).
+padre(antonio, ana).
+padre(antonio, hortensia).
+padre(david, montse).
+padre(david, david).
+madre(guadalupe,guadalupe).
+madre(guadalupe,lucio).
+madre(guadalupe,carmen).
+madre(guadalupe,guero).
+madre(guadalupe,lety).
+madre(guadalupe,alfredo).
+madre(hortensia, david).
+madre(hortensia, guero).
+madre(hortensia, ana).
+madre(hortensia, hortensia).
+madre(mguadalupe, montse).
+madre(mguadalupe, david).
